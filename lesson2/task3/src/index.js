092c7e2df97b3dd55eff6e4e56485e1a963ebe93
Artem <artemklishch@gmail.com>
2020-03-02 14:25:47 +0200

    dfg

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -5,7 +5,14 @@ const addUser = userObject => store.dispatch(addUserAction(userObject));
 
 const deleteUser = id => store.dispatch(deleteUserAction(id));
 
-addUser({id: 76, name: 'Sarah'});
+addUser({ id: 76, name: 'Sarah' });
 // console.log(store.getState());
 deleteUser(76);
 // console.log(store.getState());
+
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+  // const afterDelete = store.getState();
+  // console.log(afterDelete);
+});
