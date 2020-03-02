85359a424e4047d6e9e2780359f9cd8bf01aeba4
Artem <artemklishch@gmail.com>
2020-03-02 19:43:56 +0200

    rty

--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,5 +1,5 @@
 import store, { increment, decrement } from './counter.js';
-
+import './counter.js';
 // store.subscribe(() => console.log(store.getState()));
 // store.dispatch(increment());
 // store.dispatch(increment());
