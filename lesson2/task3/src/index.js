93f7d8a0c617adf72f5ba7341392f08d712224b2
Artem <artemklishch@gmail.com>
2020-03-02 16:07:55 +0200

    чсми

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -2,15 +2,6 @@ import store from './store.js';
 import { addUser, deleteUser } from './users.actions.js';
 
 store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+store.subscribe(() => console.log(store.getState()));
 store.dispatch(addUser({ id: 70, name: 'Dan' }));
-// console.log(store.getState());
 store.dispatch(deleteUser(76));
-// console.log(store.getState());
-
-store.subscribe(() => {
-  // addUser({ id: 76, name: 'Sarah' });
-  // const state = store.getState();
-  // console.log(state);
-  // const afterDelete = store.getState();
-  // console.log(afterDelete);
-});
