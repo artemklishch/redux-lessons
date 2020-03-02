a74c2693faa8380f6469080e88f6155bca54b311
Artem <artemklishch@gmail.com>
2020-03-02 17:39:30 +0200

    dfg

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -1,7 +1,7 @@
 import store from './store.js';
 import { addUser, deleteUser } from './users.actions.js';
 
-store.dispatch(addUser({ id: 76, name: 'Sarah' }));
 store.subscribe(() => console.log(store.getState()));
+store.dispatch(addUser({ id: 76, name: 'Sarah' }));
 store.dispatch(addUser({ id: 70, name: 'Dan' }));
 store.dispatch(deleteUser(76));
