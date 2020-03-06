60bd7153e873a05bf5db5b8771d500071adee8ad
Artem <artemklishch@gmail.com>
2020-03-06 13:23:17 +0200

    авп

--- lesson6/task2-1/src/users/UserInfo.jsx
+++ lesson6/task2-1/src/users/UserInfo.jsx
@@ -13,8 +13,8 @@ const UserInfo = ({ userData, isFetching }) => {
     <div className="user">
       <img alt="User Avatar" src={userData.avatar_url} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">{`${userData.name} from`}</span>
-        <span className="user__location">{userData.location}</span>
+        <span className="user__name">{userData.name}</span>
+        <span className="user__location">{`from ${userData.location}`}</span>
       </div>
     </div>
   );
